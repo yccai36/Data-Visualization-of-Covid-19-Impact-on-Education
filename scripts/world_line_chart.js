3a2f06c0c89422759236a6635efa6b0b12cbab1b
Zhongkai Liu <zl777@cornell.edu>
2020-05-13 23:55:51 -0400

    begin world line chart

--- scripts/world_line_chart.js
+++ scripts/world_line_chart.js
@@ -1,6 +1,16 @@
 const generateWorldLinChart = async () => {
-    data = await d3.csv("../datasets/covid_impact_education.csv");
-    console.log(data);
+    const dataOriginal = await d3.csv("../datasets/covid_impact_education.csv");
+    const dateParser = d3.timeParse("%d/%m/%Y");
+
+    let data = [];
+    let dates = [];
+
+    dataOriginal.forEach((element) => {
+        let date = element["Date"];
+    });
+
+    d1 = dateParser(date1);
+    console.log(d1);
 };
 
 generateWorldLinChart();
