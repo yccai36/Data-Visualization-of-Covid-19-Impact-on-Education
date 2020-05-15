77fd6850c75aa349ef8da2d5e9b8577cfafcf203
Zhongkai Liu <zl777@cornell.edu>
2020-05-14 20:28:01 -0400

    completed world line chart data process

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ INFO 5100 Project 2
 
 - ISO Data (https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes)[ISO.json]
 - US School Closures Data (https://www.edweek.org/ew/section/multimedia/map-coronavirus-and-school-closures.html)[Education Week]
-- World School Closures Data (https://en.unesco.org/covid19/educationresponse#)[] (UNESCO)[https://en.unesco.org/covid19/educationresponse#]
+- World School Closures Data (https://en.unesco.org/covid19/educationresponse#)[UNESCO]
--- scripts/world_line_chart.js
+++ scripts/world_line_chart.js
@@ -1,8 +1,70 @@
-const generateWorldLinChart = async () => {
-    const dataOriginal = await d3.csv("../datasets/covid_impact_education.csv");
-    const dataISO = await d3.json("../datasets/ISO.json");
+const processDataWorldLine = (dataOriginal, dataISO) => {
     const AlphaToNum = processISOData(dataISO);
-    let data = processWorldData(dataOriginal, AlphaToNum);
+    const data = processWorldData(dataOriginal, AlphaToNum);
+
+    // init data
+    let dataNational = [];
+    let dataLocalized = [];
+    let dataOpen = [];
+
+    data.forEach((element) => {
+        let date = element[0]["date"];
+        let dateString = element[0]["dateString"];
+        let countNational = 0;
+        let countLocalized = 0;
+        let countOpen = 0;
+
+        element.forEach((item) => {
+            if (item["scale"] === "National") {
+                countNational++;
+            } else if (item["scale"] === "Localized") {
+                countLocalized++;
+            } else if (item["scale"] === "Open") {
+                countOpen++;
+            }
+        });
+
+        let dataOneDayNational = {
+            date: date,
+            dateString: dateString,
+            scale: "national",
+            count: countNational,
+        };
+
+        let dataOneDayLocalized = {
+            date: date,
+            dateString: dateString,
+            scale: "localized",
+            count: countLocalized,
+        };
+
+        let dataOneDayOpen = {
+            date: date,
+            dateString: dateString,
+            scale: "open",
+            count: countOpen,
+        };
+
+        dataNational.push(dataOneDayNational);
+        dataLocalized.push(dataOneDayLocalized);
+        dataOpen.push(dataOneDayOpen);
+    });
+
+    return [dataNational, dataLocalized, dataOpen];
 };
 
-generateWorldLinChart();
+const generateWorldLineChart = async () => {
+    // load orginal data
+    const dataOriginal = await d3.csv("../datasets/covid_impact_education.csv");
+    const dataISO = await d3.json("../datasets/ISO.json");
+    // format data
+    const [dataNational, dataLocalized, dataOpen] = processDataWorldLine(
+        dataOriginal,
+        dataISO
+    );
+    console.log(dataNational);
+    console.log(dataLocalized);
+    console.log(dataOpen);
+};
+
+generateWorldLineChart();
