9b029d57104a017af9eb07730f22697d7efee1db
Yuchen Cai <yc2563@cornell.edu>
2020-05-16 18:16:24 -0400

    us-map 3rd push

--- scripts/us_map.js
+++ scripts/us_map.js
@@ -2,13 +2,16 @@
 // 1. INFO5100 Course Lectures on March 04 and March 06
 // 2. http://bl.ocks.org/mapsam/6083585
 // 3. http://bl.ocks.org/rgdonohue/9280446
+// 4. https://bl.ocks.org/d3noob/a22c42db65eb00d4e369
+// 5. https://www.d3-graph-gallery.com/graph/custom_legend.html#cont1
 
 var dateArray = [];
 var currentDateIdx = 0;
 var play = false;
 
 const usmapGenerator = async function () {
-    d3.select("#us-map-clock").html(dateArray[currentDateIdx]); // sets the inner HTML with the specified value on all selected elements
+    // sets the inner HTML with the specified value on all selected elements
+    d3.select("#us-map-clock").html(dateArray[currentDateIdx]); 
 
     const svg = d3.select("#svg3");
     const width = svg.attr("width");
@@ -18,13 +21,8 @@ const usmapGenerator = async function () {
     const mapHeight = height - margin.top - margin.bottom;
 
     const map = svg.append("g");
-    // .attr("transform","translate("+margin.left+","+margin.top+")");
 
-    // const us = await d3.json("datasets/us.json");
-    // const us = await d3.json("datasets/states-albers-10m.json");
-    const us = await d3.json("datasets/states-10m.json");
-
-    console.log("us.json loaded");
+    const us = await d3.json("datasets/us.json");
 
     // 1a. d3 projection
     var projection = d3
@@ -39,25 +37,21 @@ const usmapGenerator = async function () {
 
     // 1c. Pick out topographic features
     var states = topojson.feature(us, us.objects.states); // convert TOPOJSON to GeoJSON
-    console.log("states", states);
-    // console.log(states.features);
-    // var statesMesh = topojson.mesh(us, us.objects.states);  // mesh TopoJSON geometry and convert to GeoJSON lines.
-    // console.log(statesMesh);
 
-    // const usNames = await d3.tsv("datasets/us-state-names.tsv"); // loading US 51 states' names
-    // console.log("us-state-names data loaded", usNames);
+    const usNames = await d3.tsv("datasets/us-state-names.tsv"); // loading US 51 states' names
 
-    // var idToState = [];
-    // // generate an id for the name of each state
-    // usNames.forEach( row => {
-    //     idToState[row.id] = row.name;
-    // })
+    var idToStateName = [];
+    // generate an id for the name of each state
+    usNames.forEach( row => {
+        idToStateName[row.id] = row.code;
+    })
 
-    // // filter out an empty entry
-    // idToState =  idToState.filter( (element) =>{
-    //     return  element.length > 0;
-    // });
-    // console.log("idToState", idToState);
+    // 1d. create a tooltip
+    var tooltip = d3.select("body")
+                    .append("div")
+                    .attr("class","tooltip")
+                    .style("opacity", 0);
+                    // .text("I am the state!");
 
     // 2a. Draw a  map of states with white stroke
     console.log(states.features);
@@ -65,8 +59,8 @@ const usmapGenerator = async function () {
         .data(states.features)
         .join("path")
         .attr("class", "state")
-        .attr("id", (d) => {
-            return d.properties.name;
+        .attr("id", d => {
+            return idToStateName[d.id];
         })
         .attr("d", path)
         .style("stroke", "black")
@@ -74,41 +68,34 @@ const usmapGenerator = async function () {
         .style("stroke-width", 1)
         // displaying the secondary information of the topojson features
         .on("mouseover", function (d) {
-            var statesname = d.properties.name;
-            return (document.getElementById("sname").innerHTML = statesname); // when the mouse is on the map, display the states name
+            // when the mouse is on the map, display the states Abbrvation
+            tooltip.transition()		
+                .duration(200)		
+                .style("opacity", .8)
+            tooltip .html( "StateAbbr:" + "<br/>" + idToStateName[d.id] )
+                    .style("left",(d3.event.pageX) + "px")
+                    .style("top",(d3.event.pageY - 26) + "px");
         })
         .on("mouseout", function (d) {
-            return (document.getElementById("sname").innerHTML = null); // when the mouse if left the map, display nothing
+            // when the mouse if left the map, display nothing
+            tooltip.transition()		
+                .duration(500)		
+                .style("opacity", 0);
         });
-    // .append('title')
-    //     .text(d => d.properties.NAME);
-
-    //2b. zoom
-    // svg.call(d3.zoom().on('zoom',()=>{
-    //     map.append("g")
-    //         .attr('transform', d3.event.transform);
-    // }));
-
-    // // 2c. Make a colour scale
-    // const colorScale = d3.scaleQuantile()
-    //     .domain(0, 50, 100)
-    //     .range(["yellow","blue","red"]);
 
     // 3a. loading csv data
     const dataOriginal = await d3.csv(
-        "datasets/coronavirus-school-closures-data.csv"
-    ); // loading the original school clousore data
+        // loading the original school closure data
+        "datasets/coronavirus-school-closures-data.csv" 
+    ); 
     var data = processUSDate(dataOriginal);
-    console.log("processed data", data);
-    // console.log(data[1][1]["dateString"]);
 
-    // generate an array of beginning dats of the virus pandemic from the processed data
+    // 3b. generate an array of beginning dats of the virus pandemic from the processed data
     for (let i = 0; i < data.length; i++) {
         dateArray.push(data[i][0]["dateString"]);
     }
-    console.log(dateArray);
 
-    // animate the map
+    // 3c. animate the map
     var timer;
     d3.select("button#us-map-play").on("click", function () {
         console.log("click!");
@@ -118,7 +105,9 @@ const usmapGenerator = async function () {
             timer = setInterval(function () {
                 if (currentDateIdx < dateArray.length - 1) {
                     currentDateIdx += 1;
+                    console.log("currentDateIdx",currentDateIdx);
                     drawMap(currentDateIdx);
+                    console.log("currentDate for us-map-clock:", dateArray[currentDateIdx] );
                     d3.select("#us-map-clock").html(dateArray[currentDateIdx]);
                     play = true;
                 } else {
@@ -127,9 +116,10 @@ const usmapGenerator = async function () {
                     clearInterval(timer);
                     play = false;
                     map.selectAll("path").style("fill", "white");
-                    d3.select("#us-map-clock").html("date");
+                    d3.select("#us-map-clock").html("");
+                    drawInitialMap();
                 }
-            }, 10);
+            }, 1000);
         } else {
             console.log("play is true");
             clearInterval(timer);
@@ -138,40 +128,83 @@ const usmapGenerator = async function () {
         }
     });
 
-    // helper function for drawing the map of a specific day
+    // 4a. helper function for drawing the map of a specific day
     // the input argument is the specific date in Month Day Year
     function drawMap(date) {
-        // check if the date is out of boundary
-        // if ( date < data[0][0]["date"] || date > data[9][0]["date"]){
-        //     return ;
-        // }
-        console.log("intialize");
-        var states_ordered_clousore = [];
-        var states_recommended_clousore = [];
+        
+        var states_ordered_closure = [];
+        var states_recommended_closure = [];
 
         data[date].forEach((row) => {
             if (row.status == "ordered") {
-                states_ordered_clousore.push(row.State);
+                states_ordered_closure.push(row.stateAbbr);
             } else if (row.status == "recommended") {
-                states_recommended_clousore.push(row.State);
+                states_recommended_closure.push(row.stateAbbr);
             }
         });
 
         // color the map
         console.log("begin cloring");
-        states_ordered_clousore.forEach((id) => {
+        states_ordered_closure.forEach((id) => {
             map.select("path#" + id)
-                .style("fill", "red")
+                .style("fill", "rgb(228, 26, 28)")
                 .append("title")
-                .text("ordered clousore");
+                .text("ordered closure");
         });
 
-        states_recommended_clousore.forEach((id) => {
+        states_recommended_closure.forEach((id) => {
             map.select("path#" + id)
-                .style("fill", "blue")
+                .style("fill", "rgb(55, 126, 184)")
                 .append("title")
-                .text("recommened clousore");
+                .text("recommened closure");
         });
     }
+
+    // 4b. draw the state of the initial map
+    function drawInitialMap(){
+        map.selectAll("path.state")
+            .style("fill","lightblue");
+    }
+
+    // 4c. draw the legend
+    var legendSVG = d3.select("#legend");
+
+    // create the list of legend names
+    var legend_names = ["ordered closure", "recommended closure"];
+
+    // make the colorscale
+    var colorScale = d3.scaleOrdinal()
+                         .domain(legend_names)
+                         .range(d3.schemeSet1);
+
+    // add the square for each legend
+    legendSVG.selectAll("mysquare")
+                .data(legend_names)
+                .enter()
+                .append("rect")
+                .attr("x", 10)
+                .attr("y", function(d, i){
+                    return 50 + i*30;
+                })
+                .attr("width", 20)
+                .attr("height", 20)
+                .style("fill", d => { return colorScale(d) }  );
+
+    // add the text for each legend as the name
+    legendSVG.selectAll("mylegend")
+                .data(legend_names)
+                .enter()
+                .append("text")
+                .attr("x", 40  )
+                .attr("y", function(d, i){
+                    return 50 + i*30 + 10 ;
+                })
+                .style("fill", d => { return colorScale(d) })
+                .text( d => { return d })
+                .attr("width",50)
+                .attr("height",20)
+                .attr("text-anchor", "start")
+                .style("alignment-baseline", "middle");
+
 };
 usmapGenerator();
