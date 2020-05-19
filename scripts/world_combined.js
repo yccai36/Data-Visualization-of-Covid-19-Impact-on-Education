1eea152c1233bd1965a10ee7f4a10030c4824a2d
Zhongkai Liu <zl777@cornell.edu>
2020-05-18 22:05:32 -0400

    some fix

--- scripts/world_combined.js
+++ scripts/world_combined.js
@@ -732,8 +732,8 @@ const generateWorldMap = async function () {
 
     const dataOriginal = await d3.csv("../datasets/covid_impact_education.csv");
     const dataISO = await d3.json("../datasets/ISO.json");
-    const AlphaToNum = processISOData(dataISO);
-    let surveyData = processWorldData(dataOriginal, AlphaToNum);
+    const alphaToNum = processISOData(dataISO);
+    let surveyData = processWorldData(dataOriginal, alphaToNum);
     const tooltipData = processWorldMapTooltips(surveyData);
 
     map.selectAll("path")
